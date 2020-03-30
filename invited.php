80729ecacad009a073c509ee3d8ed89bacaac380
Silveriojmc <Silveriojmc@users.noreply.github.com>
2020-03-31 00:00:36 +0200

    V1.9.0

--- invited.php
+++ invited.php
@@ -2,12 +2,16 @@
 	
 	// Inintialize URL to the variable 
 	//$url = 'http://localhost/invitedby.php';     
-	if (isset($_GET['user'])) {
-	    echo $_GET['user'];
-	    header("Location: https://huntyiberica.typeform.com/to/FYoRS7?user=".$_GET['user']);
-	} else {
-	    header("Location: https://huntyiberica.typeform.com/to/FYoRS7");
-	}
+	// if (isset($_GET['user'])) {
+	//     echo $_GET['user'];
+	//     header("Location: https://huntyiberica.typeform.com/to/FYoRS7?user=".$_GET['user']);
+	// } else {
+	//     header("Location: https://huntyiberica.typeform.com/to/FYoRS7");
+	// }
+?>
+	<html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> <title>confina2-test-1</title> <style type="text/css"> html{ margin: 0; height: 100%; overflow: hidden; } iframe{ position: absolute; left:0; right:0; bottom:0; top:0; border:0; } </style> </head> <body> <iframe id="typeform-full" width="100%" height="100%" frameborder="0" allow="camera; microphone; autoplay; encrypted-media;" src="https://huntyiberica.typeform.com/to/FYoRS7"></iframe> <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script> </body> </html>
+<?php
+
 
 ?>
 
