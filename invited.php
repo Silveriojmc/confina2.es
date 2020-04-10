7565484d30b7a289e318671ea3b2acd3b30ddde0
Silveriojmc <Silveriojmc@users.noreply.github.com>
2020-04-10 18:40:58 +0200

    Update
    Change invited.php and unsuscribe.php

--- invited.php
+++ invited.php
@@ -9,7 +9,7 @@
 	//     header("Location: https://huntyiberica.typeform.com/to/FYoRS7");
 	// }
 ?>
-	<html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> <title>¿Estás ABURRIDO? Haz nuestro test. Recibirás el insta de la persona perfecta para tu confimaniento y se acabó el aburrimiento! Sé responsable, #QUÉDATEENCASA</title> <style type="text/css"> html{ margin: 0; height: 100%; overflow: hidden; } iframe{ position: absolute; left:0; right:0; bottom:0; top:0; border:0; } </style> </head> <body> <iframe id="typeform-full" width="100%" height="100%" frameborder="0" allow="camera; microphone; autoplay; encrypted-media;" src="https://huntyiberica.typeform.com/to/FYoRS7"></iframe> <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script> </body> </html>
+	<html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> <title>¿Estás ABURRIDO? Haz nuestro test. Recibirás el insta de la persona perfecta para tu confimaniento y se acabó el aburrimiento! Sé responsable, #QUÉDATEENCASA</title> <style type="text/css"> html{ margin: 0; height: 100%; overflow: hidden; } iframe{ position: absolute; left:0; right:0; bottom:0; top:0; border:0; } </style> </head> <body> <iframe id="typeform-full" width="100%" height="100%" frameborder="0" allow="camera; microphone; autoplay; encrypted-media;" src="https://huntyiberica.typeform.com/to/FYoRS7?user=<?php echo $_GET['user']; ?>"></iframe> <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script> </body> </html>
 <?php
 
 
